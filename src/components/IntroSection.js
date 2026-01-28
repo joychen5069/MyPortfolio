8645bf9ef04f00666f2969f9a7dedafcb7ae1a9c
Joy <joychen5069@gmail.com>
2026-01-28 15:24:12 -0500

    adding headshot image and section in intro

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -3,8 +3,16 @@ import React from 'react';
 function IntroSection(props) {
     return (
         <div className='row py-5 align-items-center intro-section' id={props.id}>
-            <div className='col-12'>
+            <div className='col-8'>
                 {props.children}
+            </div>
+            <div className='col-md-4'>
+                <img 
+                src='/images/headshot.jpg'
+                alt='Headshot'
+                className='img-fluid rounded-circle'
+                style={{ maxWidth: '250px' }}
+                />
             </div>
         </div>
     );
